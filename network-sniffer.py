f05389e7928d30e9770be703fdcd528d1d0f0ec5
Jack17kdb <jack@kali.Johnson.com>
2025-09-16 12:19:25 +0300

    first batch of scripts

---
+++ network-sniffer.py
@@ -0,0 +1,19 @@
+import scapy.all as scapy
+import argparse
+
+def network_sniffer(interface):
+    try:
+        scapy.sniff(iface=interface, store=False, prn=lambda x: x.show())
+
+    except Exception as e:
+        print('An error occurred: %s' % e)
+
+def main():
+    parser = argparse.ArgumentParser(description='Network Sniffer')
+    parser.add_argument('interface', help='Network interface to sniff')
+    args = parser.parse_args()
+
+    network_sniffer(args.interface)
+
+if __name__ == '__main__':
+    main()
